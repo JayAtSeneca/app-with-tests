3dec997c20b03b765df49298c388a6da86a1d2af
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-11-06 03:33:47 -0500

    Fix - added : after Powered By text

--- pages/index.js
+++ pages/index.js
@@ -54,11 +54,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://abc.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by:{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
